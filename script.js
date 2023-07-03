97f4355d11064ef4744e9bc5313d1117d6ebc28b
OMOLLO VANISTELROOY MASIME <111060881+OmolloRovy@users.noreply.github.com>
2023-07-03 11:32:04 +0300

    Update script.js

--- script.js
+++ script.js
@@ -66,7 +66,7 @@ function sendEmail(){
                 Password : "omollo32t",
                 To : 'omollovanistelrooy@gmail.com',
                 From : document.getElementById("email").value,
-                Subject : ,
+                Subject : document.getElementById("").value,
                 Body : "And this is the body"
             }).then(
               message => alert(message)
