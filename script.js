0bf1f1beec3c5987dccfd4ca92b0cc8d0f9c244e
OMOLLO VANISTELROOY MASIME <111060881+OmolloRovy@users.noreply.github.com>
2023-08-17 12:01:40 +0300

    Update script.js

--- script.js
+++ script.js
@@ -52,7 +52,7 @@ ScrollReveal().reveal('.home-content p, .about-content', { orgin: 'right' });
 /*================typed js===============*/
 const typed = new Typed('.multiple-text', {
 
-    strings: ['Web Developer','Data Analyst','Cyber Security Enthusiast. '],
+    strings: ['Full Stack Developer','Data Analyst','Cyber Security Enthusiast. '],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
