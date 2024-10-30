3d045ded52d64f3ef752a7d77ed9cff7cd93c89a
omolloRovy <masimevanistelrooy@gmail.com>
2024-10-30 10:08:14 +0300

    string update loop

--- script.js
+++ script.js
@@ -52,7 +52,7 @@ ScrollReveal().reveal('.home-content p, .about-content', { orgin: 'right' });
 /*================typed js===============*/
 const typed = new Typed('.multiple-text', {
 
-    strings: ['Full Stack Developer','Cyber Security Enthusiast. '],
+    strings: ['Full Stack Developer','Cyber Security Enthusiast. ', 'IT Consultant'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
